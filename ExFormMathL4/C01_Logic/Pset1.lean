b5fba0c4e735b6fd9beeeb8329918cf8a50ebe1d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-10-18 18:37:28 +0200

    Añade la 4ª demostración de 1.1.15

--- ExFormMathL4/C01_Logic/Pset1.lean
+++ ExFormMathL4/C01_Logic/Pset1.lean
@@ -757,3 +757,27 @@ example :
   apply h4
   -- ⊢ P → P
   exact fun hP => hP
+
+-- Comentario de JA: La demostración anterior se puede refactorizar como
+-- se muestra a continuación.
+
+-- Proof 4
+example :
+    (((P → Q → Q) → (P → Q) → Q) → R) →
+    ((((P → P) → Q) → P → P → Q) → R) →
+    (((P → P → Q) → (P → P) → Q) → R) →
+    R := by
+  intro _h1 h2 _h3
+  -- _h1 : ((P → Q → Q) → (P → Q) → Q) → R
+  -- h2 : (((P → P) → Q) → P → P → Q) → R
+  -- _h3 : ((P → P → Q) → (P → P) → Q) → R
+  -- ⊢ R
+  apply h2
+  -- ⊢ ((P → P) → Q) → P → P → Q
+  intro h4
+  -- h4 : (P → P) → Q
+  -- ⊢ P → P → Q
+  intro _hP1 _hP2
+  -- _hP1 _hP2 : P
+  -- ⊢ Q
+  exact h4 (fun hP => hP)
