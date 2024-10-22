f219176a3d37bc37a2d2926c2c006359a3114ede
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-10-22 11:10:39 +0200

    Añade introdución a la relación 1.3

--- ExFormMathL4/C01_Logic/Pset3.lean
+++ ExFormMathL4/C01_Logic/Pset3.lean
@@ -1,3 +1,14 @@
+-- C01_Logic/Pset3.lean
+-- Problem set 3: Negation.
+-- Gabriel Sierra Gallego.
+-- Seville, October 22, 2024
+-- ---------------------------------------------------------------------
+
+-- In this problem set, we study how to work with the `not` in Lean4.
+--
+-- It is based on [Section01logic/Sheet3.lean](https://tinyurl.com/279t67ga)
+-- from [Kevin Buzzard's course](https://tinyurl.com/26ek593r).
+
 import Mathlib.Tactic
 
 variable (P Q R : Prop)
