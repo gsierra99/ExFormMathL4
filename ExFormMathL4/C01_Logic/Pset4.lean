9b88898c296438bda0a4f2af057f4067db916a5b
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-10-22 16:59:34 +0200

    Añade introducción a la relación 1.4

--- ExFormMathL4/C01_Logic/Pset4.lean
+++ ExFormMathL4/C01_Logic/Pset4.lean
@@ -1,3 +1,14 @@
+-- C01_Logic/Pset4.lean
+-- Problem set 4: The conjunction.
+-- Gabriel Sierra Gallego.
+-- Seville, October 22, 2024
+-- ---------------------------------------------------------------------
+
+-- In this problem set, we study how to work with the conjunction in Lean4.
+--
+-- It is based on [Section01logic/Sheet4.lean](https://tinyurl.com/29cyxpjz)
+-- from [Kevin Buzzard's course](https://tinyurl.com/26ek593r).
+
 import Mathlib.Tactic
 
 variable (P Q R : Prop)
