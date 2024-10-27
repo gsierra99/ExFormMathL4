de70aa56c2de8cb4fea59184fe326459dbb2ccca
Jose A. Alonso <JoseA.Alonso@gmail.com>
2024-10-27 13:11:11 +0100

    Añade introducción a la relación 2.1

--- ExFormMathL4/C02_Reals/Pset1.lean
+++ ExFormMathL4/C02_Reals/Pset1.lean
@@ -1,4 +1,21 @@
+-- C02_Reals/Pset1.lean
+-- Problem set 1: Equalities and inequalities between numerical expressions.
+-- Gabriel Sierra Gallego.
+-- Seville, October 27, 2024
+-- ---------------------------------------------------------------------
+
+-- In this problem set, we study how to work with basic equalities and
+-- inequalities between numerical expressions in Lean4.
+--
+-- It is based on [Section01logic/Sheet4.lean](https://tinyurl.com/288azgyn)
+-- from [Kevin Buzzard's course](https://tinyurl.com/26ek593r).
+
 import Mathlib.Tactic
+
+-- ---------------------------------------------------------------------
+-- Exercise 1. Prove that
+--    2 + 2 = 4
+-- ---------------------------------------------------------------------
 
 example : (2 : ℝ) + 2 = 4 := by
   norm_num
